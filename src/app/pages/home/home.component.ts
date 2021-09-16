d0502098f68dc839d016b72921a1f311068e9abb
MichaelVelarde <michael.velarde.kubber@gmail.com>
2021-09-16 12:56:10 -0400

    .

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,32 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import {PublicationService} from "../shared/services/publication.service";
-import {HomeService} from "./home.service";
-import {filter} from "rxjs/operators";
+import { PublicationService } from '../shared/services/publication.service';
+import { HomeService } from './home.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private publicationService: PublicationService,
-    private homeService: HomeService) { }
+  publications = [];
+  constructor(
+    private publicationService: PublicationService,
+    private homeService: HomeService
+  ) {}
 
   ngOnInit(): void {
-
-    this.homeService.currentLoad().pipe(filter(s => s === true))
-    .subscribe(s => this.loadData())
+    this.homeService
+      .currentLoad()
+      .pipe(filter((s) => s === true))
+      .subscribe((s) => this.loadData());
     this.loadData();
-
   }
-
-  onShowMessage():void{
+  onShowMessage(): void {}
+  saveData(data: any): void {
+    this.publications = Object.entries(data);
+    console.log('estoy en save data', data);
   }
-  loadData(){
-    this.publicationService.getAll().subscribe(res => {
-
+  loadData() {
+    this.publicationService.getAll().subscribe((res) => {
       console.log('RES PUBLICATIONS', res);
-
-    })
+      this.saveData(res);
+    });
   }
-
 }
